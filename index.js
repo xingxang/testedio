309c86f8548c6e23c9ac83e43048487aaca6c0ef
Dmitri <perepelenkodmitri@gmail.com>
2017-11-03 19:32:57 +0000

    new branch commit

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const faker = require('faker');
 
 const input = `Value: ${faker.hacker.abbreviation()}`;
-
+// test
 fs.writeFile('test.txt', input, () => console.log('build done'));
