577a42ecfd8ad832d95c8510b07b726ebd4589cc
Dmitri <perepelenkodmitri@gmail.com>
2017-11-03 22:19:36 +0000

    fuck

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const faker = require('faker');
-
+// 222
 const input = `Value: ${faker.hacker.abbreviation()}`;
 
 fs.writeFile('test.txt', input, () => console.log('build done'));
